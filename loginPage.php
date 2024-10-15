90b27a3c9383419b5427e1daaba8cbf6ff6d9dce
Imam <imamalghifary6@gmail.com>
2024-10-15 19:07:06 +0700

    Fixing mirror issue regarding the submit button

--- loginPage.php
+++ loginPage.php
@@ -75,7 +75,7 @@
             // Your authentication logic goes here
             if($username === "admin" && $password === "admin123") {
                 $_SESSION['username'] = $username;
-                header("Location: dashboard.php"); // Redirect to dashboard page
+                header("Location: index.php"); // Redirect to dashboard page
                 exit();
             } else {
                 echo "<p class='error-message'>Invalid username or password</p>";
@@ -85,7 +85,7 @@
         <form action="" method="post">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
-            <button><a href="/Hospital_Management/index.php">Submit</a></button>
+            <input type="submit" name="login" value="Submit">
         </form>
     </div>
 </body>
